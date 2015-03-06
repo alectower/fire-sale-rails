a985974b66f03166cdec656aa74227bb7237277d
Alec Tower <alectower@gmail.com>
2015-03-06 18:59:13 -0500

    Add alert create

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -2,4 +2,20 @@ class AlertsController < ApplicationController
   def index
     render json: Alert.where(email: params[:email])
   end
+
+  def create
+    alert = Alert.new(alert_params)
+    if alert.save
+      render json: alert
+    else
+      render json: alert.errors,
+        status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def alert_params
+    params.require(:alert).permit(:email, :symbol, :price)
+  end
 end
