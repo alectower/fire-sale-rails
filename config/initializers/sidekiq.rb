05df900e8f79151b4894ea2babd94744c964e1cc
Alec Tower <alectower@gmail.com>
2015-03-08 18:09:09 -0400

    Update redis url env name

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,4 @@
-redis_url = ENV["REDISTOGO_URL"] || 'redis://localhost:6379'
+redis_url = ENV["REDIS_URL"] || 'redis://localhost:6379'
 
 Sidekiq.configure_server do |config|
   config.redis = { url: redis_url }
